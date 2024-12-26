18256c6a487c3f6b86f3b42ca8ed0ef8a28459fe
LoKias_ <diseursmat@gmail.com>
2024-12-26 18:33:29 +0100

    V2 - Gestion complète du sapin sans le pieds du sapin

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 int main(void) {
     int x, y;
-    int base, hauteur;
-    hauteur = 5;
-    for (x = 1; x <= hauteur; x++) { // Gestion du premier X + De la hauteur totale du sapin
-        for(y = 1; y <= (x * 2 - 1); y++) {
+    int espace, hauteur;
+    hauteur = 10;
+
+    for (x = 1; x <= hauteur; x++) {
+        for (espace = 1; espace <= hauteur - x; espace++) {
+            printf(" ");
+        }
+        for (y = 1; y <= (2 * x - 1); y++) {
             printf("*");
         }
         printf("\n");
     }
     getchar();
+    return 0;
 }
-
-
