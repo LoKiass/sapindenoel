ca55d060f6c97f4d065d68c403a8ade8a23a71ae
LoKias_ <diseursmat@gmail.com>
2024-12-26 19:27:49 +0100

    V3 - Gestion complète du sapin avec le pieds du sapin

--- main.c
+++ main.c
@@ -2,18 +2,22 @@
 
 int main(void) {
     int x, y;
-    int espace, hauteur;
+    int hauteur, espace;
     hauteur = 10;
-
     for (x = 1; x <= hauteur; x++) {
         for (espace = 1; espace <= hauteur - x; espace++) {
             printf(" ");
         }
-        for (y = 1; y <= (2 * x - 1); y++) {
+        for (y = 1; y <= (x * 2 - 1); y++) {
             printf("*");
         }
         printf("\n");
     }
+    for (x = 1; x <= hauteur / 3; x++) {
+        for (espace = 1; espace <= hauteur - 1; espace++) {
+            printf(" ");
+        }
+        printf("||\n");
+    }
     getchar();
-    return 0;
 }
