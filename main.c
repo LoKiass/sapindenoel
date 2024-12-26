b7dd68e43b35bbf117c333fa5f5a4ac4e764e777
LoKias_ <diseursmat@gmail.com>
2024-12-26 17:53:27 +0100

    V1 - Gestion unique des "*" sans espace

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(void) {
-
+    int x, y;
+    int base, hauteur;
+    hauteur = 5;
+    for (x = 1; x <= hauteur; x++) { // Gestion du premier X + De la hauteur totale du sapin
+        for(y = 1; y <= (x * 2 - 1); y++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    getchar();
 }
 
 
